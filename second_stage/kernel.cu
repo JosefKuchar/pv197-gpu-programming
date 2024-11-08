76282d29ad416fcb067d58605584d2792137e543
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-09 00:23:54 +0100

    buggy test

--- second_stage/kernel.cu
+++ second_stage/kernel.cu
@@ -3,12 +3,14 @@
 // https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
 // https://web.archive.org/web/20110924131401/http://www.moderngpu.com/intro/scan.html
 
-#define COLS 32
+#define COLS 16
 #define ROWS 32
 // __device__ int temp[32 * 8192];
 __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
     __shared__ int temp[COLS * ROWS];
-    int index = blockIdx.x * 32 + threadIdx.x % COLS + clients * (threadIdx.x / COLS) * 256;
+    int tx = threadIdx.x % COLS;
+    int ty = threadIdx.x / COLS;
+    int index = blockIdx.x * COLS + tx + clients * (ty) * 256;
     int val = 0;
 #pragma unroll
     for (int i = 0; i < 256; i++) {
@@ -17,27 +19,29 @@ __global__ void firstStage(int* changes, int* account, int* sum, int clients, in
         index += clients;
     }
 
-    // // Store to temp
-    // temp[threadIdx.x + threadIdx.y * 32] = val;
-    // __syncthreads();
+    // Store to temp
+    temp[tx + ty * COLS] = val;
+    __syncthreads();
 
-    // // Exclusive scan in shared memory
-    // // TODO: Do with multiple threads
-    // if (threadIdx.y == 0) {
-    //     int sum = 0;
-    //     for (int i = 0; i < 32; i++) {
-    //         int current = temp[threadIdx.x + i * 32];
-    //         temp[threadIdx.x + i * 32] = sum;
-    //         sum += current;
-    //     }
-    // }
-    // __syncthreads();
-    // // Store back to global memory
-    // index = blockIdx.x * blockDim.x + threadIdx.x + clients * threadIdx.y * 256;
-    // for (int i = 0; i < 256; i++) {
-    //     account[index] += temp[threadIdx.x + threadIdx.y * 32];
-    //     index += clients;
-    // }
+    // Exclusive scan in shared memory
+    // TODO: Do with multiple threads
+    if (ty == 0) {
+        int sum = 0;
+        for (int i = 0; i < 32; i++) {
+            int current = temp[tx + i * COLS];
+            temp[tx + i * COLS] = sum;
+            sum += current;
+        }
+    }
+    __syncthreads();
+    // Store back to global memory
+    index = blockIdx.x * COLS + tx + clients * (ty) * 256;
+    // printf("tx: %d, ty: %d, i: %d, v: %d\n", tx, ty, tx + ty * COLS, temp[tx + ty * COLS]);
+#pragma unroll
+    for (int i = 0; i < 256; i++) {
+        account[index] += temp[tx + ty * COLS];
+        index += clients;
+    }
 }
 
 void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
@@ -46,6 +50,6 @@ void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
     // Output memory errors
     cudaError_t error = cudaGetLastError();
     if (error != cudaSuccess) {
-        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+        printf("CUDA error: %s\n", cudaGetErrorString(error));
     }
 }
