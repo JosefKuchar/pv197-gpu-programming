6dfac8fde1c13ff7acb7a293474b35b5e3b7d82d
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-08 23:05:04 +0100

    new full account kernels

--- second_stage/kernel.cu
+++ second_stage/kernel.cu
@@ -21,29 +21,62 @@ __global__ void firstStage(int* changes, int* account, int clients, int periods)
         index += clients;
     }
     // Store sum for next stage
-    temp[blockIdx.x * blockDim.x + threadIdx.x + blockIdx.y * 32] = val;
+    temp[blockIdx.y + (blockIdx.x * blockDim.x + threadIdx.x) * 32] = val;
 }
 
 __global__ void secondStage() {
-    __shared__ volatile int scan[WARP_SIZE + WARP_SIZE / 2];
-    volatile int* s = scan + WARP_SIZE / 2 + threadIdx.x;
+    __shared__ volatile int scan[WARP_SIZE];
+    int tid = threadIdx.x;
+
+    // Read from global memory.
     int x = temp[threadIdx.x + blockIdx.x * blockDim.x];
-    s[0] = x;
+    scan[tid] = x;
+
+    // Run each pass of the scan.
     int sum = x;
+// #pragma unroll
+// for(int offset = 1; offset < WARP_SIZE; offset *= 2) {
+// This code generates
+//  * Advisory: Loop was not unrolled, cannot deduce loop trip count"
+// We want to use the above iterators, but nvcc totally sucks. It only
+// unrolls loops when the conditional is simply incremented.
 #pragma unroll
     for (int i = 0; i < 5; ++i) {
+        // Counting from i = 0 to 5 and shifting 1 by that number of bits
+        // generates the desired offset sequence of (1, 2, 4, 8, 16).
         int offset = 1 << i;
-        int y = s[-offset];
-        sum += y;
-        s[0] = sum;
+
+        // Add tid - offset into sum, if this does not put us past the beginning
+        // of the array. Write the sum back into scan array.
+        if (tid >= offset)
+            sum += scan[tid - offset];
+        scan[tid] = sum;
     }
     temp[threadIdx.x + blockIdx.x * blockDim.x] = sum - x;
+}
+
+__global__ void thirdStage(int* account, int clients) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x + clients * blockIdx.y * ROWS;
+    int add = temp[blockIdx.y + (blockIdx.x * blockDim.x + threadIdx.x) * 32];
+
+#pragma unroll
+    for (int i = 0; i < ROWS; i++) {
+        account[index] += add;
+        index += clients;
+    }
 }
 
 void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
     dim3 grid(clients / COLS, periods / ROWS);
     firstStage<<<grid, COLS>>>(changes, account, clients, periods);
     secondStage<<<clients, 32>>>();
+    thirdStage<<<grid, COLS>>>(account, clients);
     // sumScan2<<<clients, grid.y / 2, sizeof(int) * grid.y>>>(grid.y);
     // kernel2<<<grid, block>>>(account, clients, periods);
+
+    // Output memory errors
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error));
+    }
 }
