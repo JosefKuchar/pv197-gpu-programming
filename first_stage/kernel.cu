dec358f16bca83526e69527b5494ce6abc62098c
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-11 00:03:57 +0100

    test

--- first_stage/kernel.cu
+++ first_stage/kernel.cu
@@ -3,7 +3,7 @@ const int BLOCK_SIZE = 32;
 #define P 8
 
 __global__ void kernel(int* changes, int* account, int* sum, int clients, int periods) {
-    __shared__ volatile int shared[BLOCK_SIZE];
+    __shared__ volatile int shared[BLOCK_SIZE * P];
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int acc = 0;
     int cache[P];
@@ -16,16 +16,23 @@ __global__ void kernel(int* changes, int* account, int* sum, int clients, int pe
         for (int k = 0; k < P; k++) {
             acc += cache[k];
             cache[k] = acc;
-            shared[threadIdx.x] = acc;
+        }
+#pragma unroll
+        for (int k = 0; k < P; k++) {
+            shared[threadIdx.x + k * BLOCK_SIZE] = cache[k];
+        }
+#pragma unroll
+        for (int k = 0; k < P; k++) {
+            int idx = threadIdx.x + k * BLOCK_SIZE;
             if (threadIdx.x < 16) {
-                shared[threadIdx.x] += shared[threadIdx.x + 16];
-                shared[threadIdx.x] += shared[threadIdx.x + 8];
-                shared[threadIdx.x] += shared[threadIdx.x + 4];
-                shared[threadIdx.x] += shared[threadIdx.x + 2];
-                shared[threadIdx.x] += shared[threadIdx.x + 1];
+                shared[idx] += shared[idx + 16];
+                shared[idx] += shared[idx + 8];
+                shared[idx] += shared[idx + 4];
+                shared[idx] += shared[idx + 2];
+                shared[idx] += shared[idx + 1];
             }
             if (threadIdx.x == 0) {
-                atomicAdd(&sum[j * P + k], shared[0]);
+                atomicAdd(&sum[j * P + k], shared[idx]);
             }
         }
 #pragma unroll
