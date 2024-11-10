fcb56c0e414d4c2e42972c825fdf62d36fb3e019
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-10 23:23:15 +0100

    test

--- first_stage/kernel.cu
+++ first_stage/kernel.cu
@@ -1,11 +1,25 @@
-const int BLOCK_SIZE = 128;
+const int BLOCK_SIZE = 32;
+
+#define P 4
+
 __global__ void kernel(int* changes, int* account, int* sum, int clients, int periods) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    account[i] = changes[i];
-    atomicAdd(&sum[0], account[i]);
-    for (int j = 1; j < periods; j++) {
-        account[j * clients + i] = account[(j - 1) * clients + i] + changes[j * clients + i];
-        atomicAdd(&sum[j], account[j * clients + i]);
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int acc = 0;
+    int cache_in[P];
+    int cache_out[P];
+    for (int j = 0; j < periods / P; j++) {
+        for (int k = 0; k < P; k++) {
+            cache_in[k] = changes[index + k * clients];
+        }
+        for (int k = 0; k < P; k++) {
+            acc += cache_in[k];
+            // atomicAdd(&sum[j * P + k], acc);
+            cache_out[k] = acc;
+        }
+        for (int k = 0; k < P; k++) {
+            account[index + k * clients] = cache_out[k];
+        }
+        index += P * clients;
     }
 }
 
