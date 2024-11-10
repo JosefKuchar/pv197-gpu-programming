816c0ad3ff234b6e99889c74a57a855942af2667
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-10 19:27:14 +0100

    16tile

--- second_stage/kernel.cu
+++ second_stage/kernel.cu
@@ -3,7 +3,7 @@
 // https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
 // https://web.archive.org/web/20110924131401/http://www.moderngpu.com/intro/scan.html
 
-#define TILE 32
+#define TILE 16
 
 // __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
 //     __shared__ volatile int temp[BLOCK_COLS * ROWS * 2];
@@ -29,25 +29,26 @@
 //     }
 // }
 
-__global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
-    // __shared__ volatile int temp[BLOCK_COLS * ROWS];
+__global__ void kernel(int* changes, int* account, int* sum, int clients, int periods) {
+    __shared__ volatile int shared[TILE * (TILE + 1)];
 
     // int tx = threadIdx.x % COLS;
     // int ty = (threadIdx.x / COLS) % ROWS;
     // int xx = threadIdx.x / (COLS * ROWS);
 
     int index = threadIdx.x + blockIdx.x * TILE + clients * threadIdx.y;
-    for (int i = 0; i < 256; i++) {
-        int val = changes[index];
+    int si = threadIdx.x + threadIdx.y * TILE;
+    for (int i = 0; i < 512; i++) {
+        shared[si] = changes[index];
         __syncthreads();
-        account[index] = val;
+        account[index] = shared[si];
         index += clients * TILE;
     }
 }
 
 void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
     dim3 block(TILE, TILE);
-    firstStage<<<clients / TILE, block>>>(changes, account, sum, clients, periods);
+    kernel<<<clients / TILE, block>>>(changes, account, sum, clients, periods);
 
     // Output memory errors
     cudaError_t error = cudaGetLastError();
