2dc7247d863898a9bd910be9b5e344ddf8175ba8
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-10 18:38:05 +0100

    yet another memory test

--- second_stage/kernel.cu
+++ second_stage/kernel.cu
@@ -3,9 +3,7 @@
 // https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
 // https://web.archive.org/web/20110924131401/http://www.moderngpu.com/intro/scan.html
 
-#define BLOCK_COLS 32
-#define COLS 8
-#define ROWS 4
+#define TILE 32
 
 // __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
 //     __shared__ volatile int temp[BLOCK_COLS * ROWS * 2];
@@ -32,43 +30,24 @@
 // }
 
 __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
-    __shared__ volatile int temp[BLOCK_COLS * ROWS];
+    // __shared__ volatile int temp[BLOCK_COLS * ROWS];
 
-    int tx = threadIdx.x % COLS;
-    int ty = (threadIdx.x / COLS) % ROWS;
-    int xx = threadIdx.x / (COLS * ROWS);
+    // int tx = threadIdx.x % COLS;
+    // int ty = (threadIdx.x / COLS) % ROWS;
+    // int xx = threadIdx.x / (COLS * ROWS);
 
-    int index = tx + xx * COLS + blockIdx.x * BLOCK_COLS + clients * ty;
-
-    int sharedIndex = ty + tx * ROWS + xx * (ROWS * COLS);
-    int prev = 0;
-    for (int i = 0; i < 2048; i++) {
-        temp[sharedIndex] = changes[index] + prev;
-        if (ty < 3) {
-            temp[sharedIndex + 1] += temp[sharedIndex];
-        }
-        if (ty < 2) {
-            temp[sharedIndex + 2] += temp[sharedIndex];
-        }
-        if (ty == 0) {
-            prev = temp[sharedIndex + 3];
-        }
-        account[index] = temp[sharedIndex];
-        if (tx < 4) {
-            temp[sharedIndex] += temp[sharedIndex + 4 * 4];
-            temp[sharedIndex] += temp[sharedIndex + 4 * 2];
-            temp[sharedIndex] += temp[sharedIndex + 4];
-        }
-        if (tx == 0) {
-            atomicAdd(&sum[4 * i + ty], temp[sharedIndex]);
-        }
-        index += clients * ROWS;
+    int index = threadIdx.x + blockIdx.x * TILE + clients * threadIdx.y;
+    for (int i = 0; i < 256; i++) {
+        int val = changes[index];
+        __syncthreads();
+        account[index] = val;
+        index += clients * TILE;
     }
 }
 
 void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
-    firstStage<<<clients / BLOCK_COLS, BLOCK_COLS * ROWS>>>(changes, account, sum, clients,
-                                                            periods);
+    dim3 block(TILE, TILE);
+    firstStage<<<clients / TILE, block>>>(changes, account, sum, clients, periods);
 
     // Output memory errors
     cudaError_t error = cudaGetLastError();
