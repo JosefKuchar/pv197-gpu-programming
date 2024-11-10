688a849589a9d16a01b6efc3abfa47da60740a54
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-10 02:38:29 +0100

    warp sum

--- second_stage/kernel.cu
+++ second_stage/kernel.cu
@@ -7,8 +7,32 @@
 #define COLS 8
 #define ROWS 4
 
+// __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
+//     __shared__ volatile int temp[BLOCK_COLS * ROWS * 2];
+
+//     int tx = threadIdx.x % COLS;
+//     int ty = (threadIdx.x / COLS) % ROWS;
+//     int xx = threadIdx.x / (COLS * ROWS);
+
+//     int index = tx + xx * COLS + blockIdx.x * BLOCK_COLS + clients * ty;
+
+//     int sharedIndex = ty + tx * (ROWS + ROWS / 2) + xx * (ROWS * COLS * 2);
+//     int prev = 0;
+//     for (int i = 0; i < 2048; i++) {
+//         temp[sharedIndex] = changes[index] + prev;
+//         temp[sharedIndex + 1] += temp[sharedIndex];
+//         temp[sharedIndex + 2] += temp[sharedIndex];
+//         if (ty == 0) {
+//             prev = temp[sharedIndex + 3];
+//         }
+//         account[index] = temp[sharedIndex];
+//         // atomicAdd(&sum[4 * i + ty], temp[sharedIndex]);
+//         index += clients * ROWS;
+//     }
+// }
+
 __global__ void firstStage(int* changes, int* account, int* sum, int clients, int periods) {
-    __shared__ volatile int temp[BLOCK_COLS * ROWS * 2];
+    __shared__ volatile int temp[BLOCK_COLS * ROWS];
 
     int tx = threadIdx.x % COLS;
     int ty = (threadIdx.x / COLS) % ROWS;
@@ -16,17 +40,26 @@ __global__ void firstStage(int* changes, int* account, int* sum, int clients, in
 
     int index = tx + xx * COLS + blockIdx.x * BLOCK_COLS + clients * ty;
 
-    int sharedIndex = ty + tx * ROWS * 2 + xx * (ROWS * COLS * 2);
+    int sharedIndex = ty + tx * ROWS + xx * (ROWS * COLS);
     int prev = 0;
     for (int i = 0; i < 2048; i++) {
         temp[sharedIndex] = changes[index] + prev;
-        temp[sharedIndex + 1] += temp[sharedIndex];
-        temp[sharedIndex + 2] += temp[sharedIndex];
+        if (ty < 3) {
+            temp[sharedIndex + 1] += temp[sharedIndex];
+        }
+        if (ty < 2) {
+            temp[sharedIndex + 2] += temp[sharedIndex];
+        }
         if (ty == 0) {
             prev = temp[sharedIndex + 3];
         }
         account[index] = temp[sharedIndex];
-        // atomicAdd(&sum[4 * i + ty], temp[sharedIndex]);
+        temp[sharedIndex] += temp[sharedIndex + 4 * 4];
+        temp[sharedIndex] += temp[sharedIndex + 4 * 2];
+        temp[sharedIndex] += temp[sharedIndex + 4];
+        if (tx == 0) {
+            atomicAdd(&sum[4 * i + ty], temp[sharedIndex]);
+        }
         index += clients * ROWS;
     }
 }
