70dd9641b75329a70e7426aae3711de9b33c2c82
Josef Kuchař <josef.kuchar267@gmail.com>
2024-11-11 18:11:02 +0100

    test

--- first_stage/kernel.cu
+++ first_stage/kernel.cu
@@ -1,41 +1,46 @@
 #define BLOCK_SIZE 64
-#define P 16
+#define P 8
 
-__global__ void kernel(int* changes, int* account, int* sum) {
+__global__ void kernel(int* changes, int* account, int* sum, int clients, int periods) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int acc = 0;
     int cache[P];
+    unsigned sums[P];
     for (int j = 0; j < 8192 / P; j++) {
-        int offset = index;
 #pragma unroll
         for (int k = 0; k < P; k++) {
-            cache[k] = changes[offset];
-            offset += 8192;
+            cache[k] = changes[index + k * clients];
         }
 #pragma unroll
         for (int k = 0; k < P; k++) {
             acc += cache[k];
             cache[k] = acc;
-            int warp_sum = acc;
+            // atomicAdd(&sum[j * P + k], acc);
+        }
+#pragma unroll
+        for (int k = 0; k < P; k++) {
+            int warp_sum = cache[k];
             warp_sum += __shfl_down_sync(0xFFFFFFFF, warp_sum, 16);
             warp_sum += __shfl_down_sync(0xFFFFFFFF, warp_sum, 8);
             warp_sum += __shfl_down_sync(0xFFFFFFFF, warp_sum, 4);
             warp_sum += __shfl_down_sync(0xFFFFFFFF, warp_sum, 2);
             warp_sum += __shfl_down_sync(0xFFFFFFFF, warp_sum, 1);
-            if (threadIdx.x % 32 == 0) {
-                atomicAdd(&sum[j * P + k], warp_sum);
-            }
+            sums[k] = warp_sum;
         }
-        offset = index;
 #pragma unroll
         for (int k = 0; k < P; k++) {
-            account[offset] = cache[k];
-            offset += 8192;
+            account[index + k * clients] = cache[k];
+        }
+        if (threadIdx.x % 32 == 0) {
+#pragma unroll
+            for (int k = 0; k < P; k++) {
+                atomicAdd(&sum[j * P + k], sums[k]);
+            }
         }
         index += P * 8192;
     }
 }
 
 void solveGPU(int* changes, int* account, int* sum, int clients, int periods) {
-    kernel<<<clients / BLOCK_SIZE, BLOCK_SIZE>>>(changes, account, sum);
+    kernel<<<clients / BLOCK_SIZE, BLOCK_SIZE>>>(changes, account, sum, clients, periods);
 }
